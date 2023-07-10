d7441b2f78540e6303d86842a6f303ba823ed5bc
Jack Agtual <jaagtual@gmail.com>
2023-07-09 21:34:59 -0700

    Using environment variable for port

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express'
+import dotenv from 'dotenv'
 
+dotenv.config()
 const app = express()
 
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
 
-app.listen(8000, () => console.log('Running server'))
+app.listen(process.env.PORT, () => console.log('Running server'))
