f8f8993722ccbf80e171fe9277db06af3f923f31
Jack Agtual <jaagtual@gmail.com>
2023-07-09 22:30:50 -0700

    Adding /place endpoint

--- dist/index.js
+++ dist/index.js
@@ -4,8 +4,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var dotenv_1 = __importDefault(require("dotenv"));
+var axios_1 = __importDefault(require("axios"));
+var cors = require('cors');
+dotenv_1.default.config();
 var app = (0, express_1.default)();
+app.use(cors());
 app.get('/', function (req, res) {
     res.send('Hello world');
 });
-app.listen(8000, function () { return console.log('Running server'); });
+app.get('/place', function (req, res) {
+    var _a = req.query, query = _a.query, type = _a.type;
+    axios_1.default
+        .get("https://maps.googleapis.com/maps/api/place/textsearch/json?query=".concat(query, "&key=").concat(process.env.GOOGLE_MAPS_API_KEY, "&type=").concat(type, "&radius=50000"))
+        .then(function (apiRes) {
+        res.json(apiRes.data);
+    });
+});
+app.listen(process.env.PORT, function () { return console.log('Running server'); });
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import axios from 'axios'
+const cors = require('cors')
 
 dotenv.config()
 const app = express()
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.get('/place', (req, res) => {
+  const { query, type } = req.query
+
+  axios
+    .get(
+      `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}&key=${process.env.GOOGLE_MAPS_API_KEY}&type=${type}&radius=50000`
+    )
+    .then((apiRes) => {
+      res.json(apiRes.data)
+    })
 })
 
 app.listen(process.env.PORT, () => console.log('Running server'))
