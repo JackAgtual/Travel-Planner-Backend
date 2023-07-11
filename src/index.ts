4744639c884c08099eb0c9748d0130b90ce4380a
Jack Agtual <jaagtual@gmail.com>
2023-07-10 21:39:35 -0700

    Using async/await for /place endpoint; adding types for query params

--- src/index.ts
+++ src/index.ts
@@ -7,20 +7,24 @@ dotenv.config()
 const app = express()
 app.use(cors())
 
+type placeQueryParams = {
+  query: string
+  type: string
+}
+
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
 
-app.get('/place', (req, res) => {
-  const { query, type } = req.query
+app.get('/place', async (req, res) => {
+  const { query, type } = req.query as placeQueryParams
 
-  axios
-    .get(
-      `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}&key=${process.env.GOOGLE_MAPS_API_KEY}&type=${type}&radius=50000`
-    )
-    .then((apiRes) => {
-      res.json(apiRes.data)
-    })
+  const apiRes = await axios.get(
+    `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}&key=${process.env.GOOGLE_MAPS_API_KEY}&type=${type}&radius=50000`
+  )
+  res.json(apiRes.data)
 })
 
-app.listen(process.env.PORT, () => console.log('Running server'))
+app.listen(process.env.PORT, () =>
+  console.log(`Running server on http://localhost${process.env.PORT}`)
+)
